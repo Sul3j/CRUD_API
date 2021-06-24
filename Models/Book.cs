3bbb0c9e1a6bd23c49fa5ecc6ee82f5ec3760e48
Szymon Sulejczak <55898132+Sul3j@users.noreply.github.com>
2021-06-24 10:44:31 +0200

    ADD: Book model

---
+++ Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryApi.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public double Prize { get; set; }
+        public bool HardCover { get; set; }
+    }
+}
